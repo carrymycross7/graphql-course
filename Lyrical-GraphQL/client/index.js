8f3bd956922950352b4fb50a0728f26973bc267e
carrymycross7 <threet.ldustin@gmail.com>
2024-02-13 22:16:35 -0600

    setup react, apollo, and mongodb components

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-client";
+import {ApolloProvider} from "react-apollo";
+import SongList from "./components/SongList";
+
+const CLIENT = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+      <ApolloProvider client={CLIENT}>
+        <SongList />
+      </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
