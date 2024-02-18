f55baf7a75c1785ec19dc95414a85ac006ac576f
carrymycross7 <threet.ldustin@gmail.com>
2024-02-17 23:23:34 -0600

    changes from sections

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-client";
+import {ApolloError, ApolloProvider} from "react-apollo";
 
+const client = new ApolloClient({});
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+      <ApolloProvider client={client}>
+        <div>
+          Lyrical
+        </div>
+      </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
+    <Root />,
+    document.querySelector('#root')
 );
