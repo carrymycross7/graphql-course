6853bd7ba5f5dcf74bbb7c88f0c09e74eb49bb69
carrymycross7 <threet.ldustin@gmail.com>
2024-02-18 19:40:13 -0600

    add state setup, add new page for song entry

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute} from "react-router";
 import ApolloClient from "apollo-client";
 import {ApolloError, ApolloProvider} from "react-apollo";
 import gql from 'graphql-tag';
 import SongList from "./components/SongList";
-
+import App from './components/App';
+import SongCreate from "./components/SongCreate";
 const CLIENT = new ApolloClient({});
 
 const Root = () => {
   return (
       <ApolloProvider client={CLIENT}>
-        <SongList />
+        <Router history={hashHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute components={SongList} />
+                <Route path="song/new" component={SongCreate} />
+            </Route>
+        </Router>
       </ApolloProvider>
   )
 };
